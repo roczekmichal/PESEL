b0594dfaec67a7f022036a511618d1730a57c282
roczekmichal <mich.rocz@gmail.com>
2020-12-04 21:14:32 +0100

    int main, pobranie danych i wyswietlenie informacji

---
+++ pesel.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <array>
+#include "funckje.h"
+
+
+int main()
+{
+    int cyfra;
+    std::array<int, 11> pesel;
+    std::cout << "Prosze podac swoj pesel po jednej cyfrze: " << std::endl;
+    for (int i = 0; i < size(pesel); i++)
+    {
+        std::cin >> cyfra;     
+        pesel[i] = cyfra;
+    }
+
+
+    std::cout << "-----------------------------------------\n";
+
+    wyswietl_pesel(pesel);
+
+    std::cout << "-----------------------------------------\n";
+
+    if (czy_poprawny(pesel)==true)
+        std::cout << "Twoj pesel jest poprawny\n";
+    else
+        std::cout << "Twoj pesel nie jest poprawany!\n";
+
+    data_urodzenia(pesel);
+       
+
+    return 0;
+}
+
